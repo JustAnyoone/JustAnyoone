a7060b9c8e5bdee42422fe3346a0d078dcc0386b
Portilho <rhyan.100pilot@gmail.com>
2026-08-21 09:06:05 -0300

    Add files via upload

---
+++ Academico/Semestre-02/Programação Orientada a Objetos/PrimeiraLista-20260821T115938Z-1-001/PrimeiraLista/Att5_Computador.java
@@ -0,0 +1,36 @@
+package POO;
+
+public class Att5_Computador {
+    String marca;
+    String modelo;
+    String processador;
+    int RAM;
+    int armazenamneto;
+    boolean power; // true = Ligado , false = DESLIGADO
+
+    public void ligar(){
+        if (power == false) {
+            power = true;
+            System.out.println("Computador foi ligado com sucesso");
+        }
+        else{System.out.println("Computador já esta ligado");}
+    }
+//------------------------------------------------------------------
+    public void desligar(){
+        if (power == true) {
+            power = false;
+            System.out.println("Computador Desligado com sucesso");
+        }
+        else{System.out.println("O computador já foi desligado");}
+    }
+//------------------------------------------------------------------
+    public void exibirConfig(){
+        System.out.println("Marca:" + marca);
+        System.out.println("Modelo:"+ modelo);
+        System.out.println("Processador: "+ processador);
+        System.out.println("Quantidade de memória RAM:" + RAM + "GB");
+        System.out.println("Capacidade de armazenamento:" + armazenamneto + "GB");
+        System.out.println("Power:" + (power ? "ligado" : "desligado")); // mensagem abaixo ta explicado
+        // se for true a na mensagem vai aparecer ligado, se for false vai aparecer desligado
+    }
+}
