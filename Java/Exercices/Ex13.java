a8bfd7b970853915c96db974a6f5263366fc9844
Portilho <rhyan.100pilot@gmail.com>
2026-08-26 17:49:50 -0300

    Add Files Exercices

---
+++ Java/Exercices/Ex13.java
@@ -0,0 +1,30 @@
+package Exercices;
+
+public class Ex13 {
+    public static void main(String[] args) {
+        printNumberInWord(0);
+        printNumberInWord(1);
+        printNumberInWord(2);
+        printNumberInWord(9);
+        printNumberInWord(22);
+    }
+
+    public static void printNumberInWord(int number){
+
+        String StringNumber = switch (number) {
+            case 0 -> {yield "ZERO"; }
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "OTHER";
+        };
+
+        System.out.println(StringNumber);
+    }
+}
