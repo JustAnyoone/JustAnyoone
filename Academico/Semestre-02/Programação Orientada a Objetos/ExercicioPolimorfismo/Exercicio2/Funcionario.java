5de2b10f6b7f1eea5d25d9c38960e9c82dd41c1e
Portilho <rhyan.100pilot@gmail.com>
2026-08-31 17:36:30 -0300

    Delete Academico/Semestre-02/Programação Orientada a Objetos directory

--- Academico/Semestre-02/Programação Orientada a Objetos/ExercicioPolimorfismo/Exercicio2/Funcionario.java
+++
@@ -1,8 +0,0 @@
-package ExercicioPolimorfismo.Exercicio2;
-
-public class Funcionario {
-
-    String nome = "Marcelo";
-    double salario = 3000;
-    
-}
--- Academico/Semestre-02/Programação Orientada a Objetos/ExercicioPolimorfismo/Exercicio2/Gerente.java
+++
@@ -1,9 +0,0 @@
-package ExercicioPolimorfismo.Exercicio2;
-
-public class Gerente extends Funcionario {
-
-    public double calcularSalario(double salario){
-       
-        return salario = salario * 1.20;
-    }
-}
